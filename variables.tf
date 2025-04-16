87d7d8aebb2168c4f45296b3ee84413a8402867e
Maneesh <maneesh4_u@yahoo.co.in>
2025-04-16 14:06:32 +0530

    Initial commit - Adding Terraform code

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS Region"
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+}
+
+variable "key_name" {
+  description = "SSH Key Name"
+}
+
